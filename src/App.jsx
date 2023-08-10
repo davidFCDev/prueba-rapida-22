5494c60ca30c9c8744496308a6a9bce3fc935f54
David Fernández <davidfernandezcomesana@gmail.com>
2023-08-10 18:33:11 +0200

    render mock

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,47 @@
 import "./App.css";
+import responseMovies from "./mocks/with-results.json";
 
 function App() {
-  return <div className="page">
-    <header>
-      <h1>Buscador de películas</h1>
-      <form className="form">
-        <input type="text" className="input" placeholder="Escribe aquí tu película favorita" />
-        <button type="submit" className="button">Buscar</button>
-      </form>
-    </header>
+  const movies = responseMovies.Search;
+  const hasMovies = movies?.length > 0;
 
-    <main>Lista de películas</main>
-  </div>;
+  return (
+    <div className="page">
+      <header>
+        <h1>Buscador de películas</h1>
+        <form className="form">
+          <input
+            type="text"
+            className="input"
+            placeholder="Escribe aquí tu película favorita"
+          />
+          <button type="submit" className="button">
+            Buscar
+          </button>
+        </form>
+      </header>
+
+      <main>
+        {hasMovies ? (
+          <ul className="movies">
+            {movies.map((movie) => {
+              return (
+                <li key={movie.imdbID} className="movie">
+                  <h2>{movie.Title}</h2>
+                  <p>{movie.Year}</p>
+                  <img src={movie.Poster} alt={movie.Title} />
+                </li>
+              );
+            })}
+          </ul>
+        ) : (
+          <p className="empty">
+            No hay películas que coincidan con la búsqueda
+          </p>
+        )}
+      </main>
+    </div>
+  );
 }
 
 export default App;
