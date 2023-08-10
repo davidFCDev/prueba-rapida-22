24423ebea2a035d5758e703b65c0aa3bf3a3dd10
David Fernández <davidfernandezcomesana@gmail.com>
2023-08-10 18:36:33 +0200

    hook useSearch

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
+import { useState } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies";
 import responseMovies from "./mocks/with-results.json";
+import { useRef } from "react";
+import { useEffect } from "react";
+
+function useSearch() {
+  const [search, updateSearch] = useState("");
+  const [error, setError] = useState(null);
+  const isFirstInput = useRef(true);
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === "";
+      return;
+    }
+    if (search === "") {
+      setError("No se puede buscar una película vacía");
+      return;
+    }
+
+    if (search.length < 3) {
+      setError("La búsqueda debe tener al menos 3 caracteres");
+      return;
+    }
+
+    setError(null);
+  }, [search]);
+
+  return { search, updateSearch, error };
+}
 
 function App() {
+  const { search, updateSearch, error } = useSearch();
   const movies = responseMovies.Search;
 
   const mappedMovies = movies.map((movie) => {
